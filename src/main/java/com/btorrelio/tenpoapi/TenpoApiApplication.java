3a08b102096e01abf483dd7e8959ebd59bcce605
brunotorrelio <brunotorrelio@gmail.com>
2022-07-14 01:03:21 -0300

    fist commit

---
+++ src/main/java/com/btorrelio/tenpoapi/TenpoApiApplication.java
@@ -0,0 +1,13 @@
+package com.btorrelio.tenpoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenpoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/btorrelio/tenpoapi/TenpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btorrelio.tenpoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenpoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
