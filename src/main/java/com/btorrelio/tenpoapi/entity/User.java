837a8e8049f1c52affdbecf79436abf673fa51e1
brunotorrelio <brunotorrelio@gmail.com>
2022-07-20 09:20:08 -0300

    added dockerfila and docker compoase

--- src/main/java/com/btorrelio/tenpoapi/entity/User.java
+++ src/main/java/com/btorrelio/tenpoapi/entity/User.java
@@ -19,8 +19,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;
-
     private String username;
 
     private String password;
